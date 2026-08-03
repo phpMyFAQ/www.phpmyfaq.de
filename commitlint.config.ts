e4dcb094e745caf8dbff91674d6c1e02c545933c
Thorsten Rinne <thorsten@phpmyfaq.de>
2026-08-03 07:52:50 +0200

    build: fix commitlint config and drop ignored pnpm block

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,7 +2,6 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
-  parserPreset: 'conventional-changelog-atom',
   formatter: '@commitlint/format',
   rules: {
     'type-enum': [
